5daa94d03921a86dae96782d2cbdc94d15eb25b4
Tim's Macbook <th2n10s4u@gmail.com>
2022-01-31 22:35:32 -0800

    initial commit

---
+++ EMDR.js
@@ -0,0 +1,55 @@
+var canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+
+var c = canvas.getContext('2d');
+
+var x = Math.random() * innerWidth;
+//var y = Math.random() * innerHeight;
+var y = 500;
+var dx = document.getElementById("Range");    //   <- isn't fucking working           // Changes the speed of the object 
+//var dy = (Math.random() - 0) * 8;
+var radius = 30;
+function animate() {
+    window.requestAnimationFrame(animate);
+    c.clearRect(0, 0,  innerWidth, innerHeight);
+
+    c.beginPath();
+    c.strokeStyle = 'yellow';
+    c.lineWidth = 10;
+    c.fillStyle = 'darkblue';
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
+    c.stroke();
+
+    if (x + radius > innerWidth || x - radius < 0) {
+        dx = -dx;
+    }
+    /*if (y + radius > innerHeight || y - radius < 0) {
+        dy = -dy;
+    }*/
+    x += dx;
+   // y += dy;
+}
+animate();
+
+// Slider // 
+// Slider // 
+// Slider // 
+
+const slideValue = document.querySelector("span");
+      const inputSlider = document.querySelector("input");
+      inputSlider.oninput = (()=>{
+        let value = inputSlider.value;
+        slideValue.textContent = value;
+        slideValue.style.left = (value/1) + "%";
+        slideValue.classList.add("show");
+      });
+      inputSlider.onblur = (()=>{
+        slideValue.classList.remove("show");
+      });
+
+    // Slider // 
+    // Slider // 
+    // Slider // 
